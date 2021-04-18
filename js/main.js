561779e933fd010b6351e1afa29aa64976363c12
rahmanoff <rahmanoff@gmail.com>
2021-04-18 15:18:34 +0300

    Responsive Feedback

--- js/main.js
+++ js/main.js
@@ -16,9 +16,28 @@ $(function() {
         dots: true,
         slidesToShow: 4,
         slidesToScroll: 1,
-        autoplay: true
+        autoplay: true,
+        responsive: [{
+                breakpoint: 1141,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 846,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 586,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
 
-    })
+        ]
+    });
     $('.menu__btn').on('click', function() {
         $('.menu__list').toggleClass('menu__list--active')
     })
